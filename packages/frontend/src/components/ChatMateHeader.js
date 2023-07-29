d430a137a1a2bea244f6cfe2b4d43095d2e9e3ee
Lorenzo-Mariano <hhumminah@gmail.com>
2023-07-30 02:42:14 +0800

    Removed unnecessary component

--- packages/frontend/src/components/ChatMateHeader.js
+++
@@ -1,9 +0,0 @@
-export default function ChatMateHeader(props) {
-	return (
-		<div className='p-4' >
-			<div className='text-2xl p-1 border-bottom-2 border-primary'>
-				{props.selectedFriend}
-			</div>
-		</div>
-	)
-}
